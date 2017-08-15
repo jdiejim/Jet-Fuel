6cb6eca20e3ee839976ca68e14f42a9e893e74ed
Juan Diego Jimenez <jdiejim@gmail.com>
2017-08-14 21:43:59 -0600

    add header with style and tab functionality

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import $ from 'jquery';
 import './styles/index.scss';
+
+$('.nav').on('click', '.tab', toggleTab);
+
+function toggleTab(e) {
+  const id = $(e.target).prop('id');
+
+  $('.tab').removeClass('active-tab');
+  $(`#${id}`).addClass('active-tab');
+}
