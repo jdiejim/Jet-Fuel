41a74782153f3a5edae043b72acd188eec08c2ad
Juan Diego Jimenez <jdiejim@gmail.com>
2017-08-18 11:18:31 -0600

    add dummy test

---
+++ test/routes.spec.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const expect = chai.expect;
+const should = chai.should();
+// const chaiHttp = require('chai-http');
+const server = require('../server');
+
+// chai.use(chaiHttp);
+
+describe('Test', () => {
+  expect(2).to.equal(2)
+})
