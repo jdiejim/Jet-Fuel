f40bb02c44c2b0eec765976fbede07cb495d9ba6
Juan Diego Jimenez <jdiejim@gmail.com>
2017-08-21 01:02:05 -0600

    add server comments

--- models/Folder.js
+++ models/Folder.js
@@ -1,15 +1,18 @@
-const environment = process.env.NODE_ENV || 'development';
-const configuration = require('../knexfile')[environment];
-const db = require('knex')(configuration);
+const environment = process.env.NODE_ENV || 'development'; // sets the environment for the database
+const configuration = require('../knexfile')[environment]; // configures the database based on the environment
+const db = require('knex')(configuration); // sets dabase with configuration
 
+// this function returns all folders from the database
 exports.getFolders = () => {
   return db('folders').orderBy('created_at', 'asc').select();
 }
 
+// this function creates a new folder in the database with the parameters passed in the controller
 exports.createFolder = (folder) => {
   return db('folders').insert(folder, ['id', 'name']);
 }
 
+// this function destroys a folder in the database with the parameters passed in the controller
 exports.deleteFolder = (id) => {
   return db('paths').where({ folder_id: id }).del()
     .then(() => db('folders').where({ id }).del())
--- router.js
+++ router.js
@@ -1,12 +1,16 @@
 const express = require('express');
-const router = express.Router();
-const foldersController = require('./controllers/foldersController');
-const pathsController = require('./controllers/pathsController');
+const router = express.Router(); // Sets chaiable routes
+const foldersController = require('./controllers/foldersController'); // brings controller functions
+const pathsController = require('./controllers/pathsController'); // brings controller functions
 
-router.get('/folders', foldersController.index);
-router.post('/folders', foldersController.create);
-router.delete('/folders', foldersController.delete);
-router.get('/folders/:id/paths', pathsController.index);
-router.post('/folders/:id/paths', pathsController.create);
+// api/v1 routes
+// All endpoints reponses are handled by each of the specified controllers
+router.get('/folders', foldersController.index); // Get endpoint for getting all folders
+router.post('/folders', foldersController.create); // Post endpoint for creating a new folder
+router.delete('/folders', foldersController.delete); // Delete enpoint for destroying a folder
+router.get('/folders/:id/paths', pathsController.index); // Get endpoint for getting all paths for a specific folder
+router.post('/folders/:id/paths', pathsController.create); // Post endpoint for creating new path for a specific foler
 
 module.exports = router;
+
+// Next files to check is controllers/foldersController.js
